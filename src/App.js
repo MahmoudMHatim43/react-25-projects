a0f91adbb20bf88b1c37434c703af87ef4a23bbc
MahmoudMHatim43 <mahmoudmhatim@gmail.com>
2024-07-11 14:43:13 +0300

    Rating Stars
    🚀✅ Slide and click to rate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Accordian from "./components/Accordian";
 import RandomColor from "./components/RandomColor/RandomColor";
+import StarRating from "./components/StarRating/StarRating";
 function App() {
   return (
     <div className="app-container">
-      <RandomColor />
+      <StarRating stars={5} />
     </div>
   );
 }
---
+++ src/components/StarRating/StarRating.js
@@ -0,0 +1,27 @@
+import "./star-rating.css";
+import { useState } from "react";
+import { FaStar } from "react-icons/fa";
+
+export default function StarRating({ stars = 5 }) {
+  const [rating, setRating] = useState(0);
+  const [done, setdone] = useState(false);
+  function rate(index) {
+    setRating(index);
+  }
+  return (
+    <div className="stars-container">
+      {[...Array(stars)].map((star, index) => (
+        <FaStar
+          key={index}
+          onMouseEnter={() => (done ? "" : rate(index + 1))}
+          onMouseLeave={() => (done ? "" : rate(0))}
+          onClick={() => {
+            rate(index + 1);
+            setdone(!done);
+          }}
+          className={rating > index ? "yellow-star" : ""}
+        />
+      ))}
+    </div>
+  );
+}
