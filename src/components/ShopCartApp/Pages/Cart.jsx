61d367914e7a771225dc64406327e85d60cb33e7
MahmoudMHatim43 <mahmoudmhatim@gmail.com>
2024-07-24 17:12:13 +0300

    app completed

--- src/components/ShopCartApp/Pages/Cart.jsx
+++ src/components/ShopCartApp/Pages/Cart.jsx
@@ -11,11 +11,11 @@ function Cart() {
   }, [cart]);
   return (
     <div className="bg-gray-300 overflow-hidden min-h-screen w-full">
-      <span className="border-b-2 border-gray-900 text-2xl p-3 m-10">
-        Total Price ${totalPrice}
-      </span>
       {cart && cart.length > 0 ? (
         <div className="min-h-screen grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-6xl mx-auto p-3">
+          <span className="border-b-2 border-gray-900 text-2xl p-3 m-10">
+            Total Price ${totalPrice.toFixed(2)}
+          </span>
           {cart.map((item) => (
             <ProductBox key={item.id} data={item} />
           ))}
